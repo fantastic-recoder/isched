08e300e3ce7c6575c3c641ba7a2dfb10837e26f7
Peter Grobarcik <coder.peter.grobarcik@gmail.com>
2023-10-29 17:21:40 +0100

    feature: work in progress on Ubuntu/Docker build.

--- configure.py
+++ configure.py
@@ -5,6 +5,6 @@ import pathlib
 myBuildDir = pathlib.Path("cmake-build-debug")
 if not myBuildDir.exists():
     myBuildDir.mkdir()
-os.system("python3 -m conan install . -of cmake-build-debug -s build_type=Debug --build=missing")
+    os.system("conan2 install . -of cmake-build-debug -s build_type=Debug --build=missing")
 os.system("cmake . -B ./cmake-build-debug -G Ninja -DCMAKE_TOOLCHAIN_FILE=cmake-build-debug/conan_toolchain.cmake -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_BUILD_TYPE=Debug")
 os.system("cmake --build ./cmake-build-debug/")
