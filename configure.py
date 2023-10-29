7aabab8bc360b0903d929a59cda06d1cd36a18d4
Peter Grobarcik <coder.peter.grobarcik@gmail.com>
2023-10-29 18:07:06 +0100

    fix: try to run conan2 from docker image.

--- configure.py
+++ configure.py
@@ -2,9 +2,13 @@
 import os
 import pathlib
 
-myBuildDir = pathlib.Path("cmake-build-debug")
-if not myBuildDir.exists():
-    myBuildDir.mkdir()
-    os.system("conan install . -of cmake-build-debug -s build_type=Debug --build=missing")
-os.system("cmake . -B ./cmake-build-debug -G Ninja -DCMAKE_TOOLCHAIN_FILE=cmake-build-debug/conan_toolchain.cmake -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_BUILD_TYPE=Debug")
-os.system("cmake --build ./cmake-build-debug/")
+if __name__ == '__main__':
+    myBuildDir = pathlib.Path("cmake-build-debug")
+    if not myBuildDir.exists():
+        myBuildDir.mkdir()
+        myRetVal = os.system("~/.local/bin/conan install . -of cmake-build-debug -s build_type=Debug --build=missing")
+        if myRetVal != 0:
+            myBuildDir.rmdir()
+            exit(myRetVal)
+    os.system("cmake . -B ./cmake-build-debug -G Ninja -DCMAKE_TOOLCHAIN_FILE=cmake-build-debug/conan_toolchain.cmake -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_BUILD_TYPE=Debug")
+    os.system("cmake --build ./cmake-build-debug/")
