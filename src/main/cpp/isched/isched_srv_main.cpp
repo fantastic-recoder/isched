c06555207bcea6920eddad90120a5350409ea6d3
FantasticRecoder <coder.peter.grobarcik@gmail.com>
2025-11-06 20:34:14 +0100

    feat: Add `README.md` and project dependencies for Apollo GraphQL server setup
    - Introduced a detailed README describing the demo Apollo GraphQL server.
    - Added instructions for installation, usage, and query samples.
    - Updated `package-lock.json` with Apollo Server, GraphQL tools, and associated dependencies.

--- src/main/cpp/isched/isched_srv_main.cpp
+++ src/main/cpp/isched/isched_srv_main.cpp
@@ -24,9 +24,11 @@ int main(const int, const char **) {
     mySvc.addResolver
     (make_shared<SingleActionResolver>
         (EHttpMethods::GET, "/test", "Resolver ansver"));
+    /*
     mySvc.addResolver
     (make_shared<DocRootResolver>
         (string("/path"), path{"/home/grobap/Documents/100_Projects/isched/isched/client"}));
+    */
     spdlog::debug("run ->");
     mySvc.run();
     return EXIT_SUCCESS;
